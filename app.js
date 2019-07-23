d1845bf1867f42f4afb6bfb265fff73577501dc2
Shichao Zhang <51226865+t-shizha@users.noreply.github.com>
2019-07-23 17:58:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+addScript();
 
 var div = document.createElement('div');
-div.setAttribute("id", "result");
+div.setAttribute("id", "real-api-usage");
 document.getElementById("main").appendChild(div);
+
 var uid = getUidName();
 getFromWebAPI(uid, div, renderRequestResult);
 
-
-
 function getFromWebAPI(uid, resultDiv, callback) {
-
     var xHttp = new XMLHttpRequest();
-    xHttp.onreadystatechange = function () {
-        if (xHttp.readyState == 4 && xHttp.status == 200)
-            callback(resultDiv, this.responseText);
+    xHttp.onreadystatechange = function (e) {
+        if (xHttp.readyState == 4){
+            if(xHttp.status == 200){
+                callback(resultDiv, this.responseText);
+            }
+            else{
+                console.log("Fail to load code snippet for uid:" + uid);
+                console.log(e);
+            }
+        }
     };
     xHttp.open("get", "https://realapiusage.azurewebsites.net/api/" + uid);
     xHttp.send();
@@ -26,38 +32,114 @@ function getUidName() {
     }
 }
 
+var realApiUsageCount = 0;
+var realApiUsageCurrent = 0;
 function renderRequestResult(resultDiv, responses) {
     try {
+        // set raw content
         var info = JSON.parse(responses);
         var numOfItems = info.length;
-        var content = "";
+        realApiUsageCount = numOfItems;
+        realApiUsageCurrent = 1;
+        var content = "<hr/><h2>Real API Usage :</h2>";
+        content += "<p>This is the real code snippet (from public github repo) that uses this API:</p>";
+        content += "<div><button class='button is-primary' onclick='previousSnippet()'>👈 Previous Snippet</button>";
+        content += "<button style='position: absolute; left: 45%; transform: translateX(-50%)' class='button is-success'>👍</button>";
+        content += "<button style='position: absolute; left: 55%; transform: translateX(-50%)' class='button is-info'>👎</button>";
+        content += "<button style='float: right' class='button is-primary' onclick='nextSnippet()'>Next Snippet 👉</button></div>"
         for (var i = 0; i < numOfItems; ++i) {
-            var startLine = info[i].startLineInCodeSnippet;
-            var endLine = info[i].endLineInCodeSnippet;
-            var invoExpression = info[i].invoExpression;
             var gitUrl = info[i].gitUrl;
-            var url = info[i].url;
-            content += "<h2>Sample " + (i + 1) + ":</h2>";
-            content += "<h4>Start-End Line: " + startLine + "-" + endLine + "</h4>";
-            content += "<h4>Invocation Expression: " + invoExpression + "</h4>";
-            content += "<a target='_blank' href='" + gitUrl + "'>See Complete Code on Github!</a><br>";
             var codesnippet = info[i].codeSnippet;
-            
-            content += "<pre class='wrap'><code class='csharp lang-csharp' dir='ltr'>";
-            content += codesnippet;
-            content += "</code></pre><br>";
+            var start = info[i].startLineInCodeSnippet;
+            var end = info[i].endLineInCodeSnippet;
+
+            content += `<div id='real-api-usage-${i + 1}'>`;
+            content += `<pre class='wrap'><code class='csharp lang-csharp' dir='ltr' highlight-lines='${start}-${end}' giturl='${gitUrl}'>`;
+            content += deIndent(codesnippet);
+            content += "</code></pre>";
+            content += "</div>";
         }
-
         resultDiv.innerHTML = content;
-        var render = document.createElement('script');
-        render.setAttribute('src', '/_themes/docs.theme/master/en-us/_themes/scripts/8fb53e85.index-docs.js');
-        document.body.appendChild(render);
-        var s2 = document.createElement('script');
-        s2.setAttribute('src', 'https://www.google-analytics.com/analytics.js');
-        document.body.appendChild(s2);
 
+        // set hidden
+        updateSnippet();
+
+        // add real api usage customized styles
+        window.addEventListener('real-api-usage-highlighted', addGitUrl, true);
+        
+        // leverage existing code to highlight, add code header
+        var event = new Event('real-api-usage-loaded');
+        window.dispatchEvent(event);
     }
-    catch{
+    catch(e){
+        console.log(e);
         resultDiv.innerHTML = "No Smaple Code!";
+    }
+}
+
+function addScript(){ 
+    var script = document.createElement('script');
+    script.innerHTML = `
+${previousSnippet.toString()}
+${nextSnippet.toString()}
+${updateSnippet.toString()}
+`;
+    document.body.appendChild(script);
+}
+
+
+function previousSnippet(){ 
+    realApiUsageCurrent--;
+    if(realApiUsageCurrent <= 0) realApiUsageCurrent = 1;
+    updateSnippet();
+}
+function nextSnippet(){ 
+    realApiUsageCurrent++;
+    if(realApiUsageCurrent > realApiUsageCount) realApiUsageCurrent = realApiUsageCount;
+    updateSnippet();
+}
+function updateSnippet(){
+    var snippets = document.querySelectorAll("div[id^='real-api-usage-']");
+    snippets.forEach(snippet => {
+        snippet.hidden = false;
+    });
+    var current = document.querySelector(`div#real-api-usage-${realApiUsageCurrent}`);
+    current.hidden = true;
+}
+
+function addGitUrl() {
+    var codes = document.querySelectorAll("div#real-api-usage code[giturl]");
+    for (var i = 0; i < codes.length; ++i) {
+        var code = codes[i];
+        var gitUrl = code.getAttribute("giturl");
+        var header = code.parentElement.previousElementSibling;
+
+        // git name
+        var lang = header.querySelector("span.language");
+        // https://github.com/Azure-Samples/gov-intelligent-mission/blob/master
+        var name = gitUrl.slice(19, gitUrl.indexOf('blob') - 1);
+        lang.innerHTML = lang.innerHTML + " - from " + name;
+
+        // git url
+        var button = header.querySelector("button");
+        var gitLink = document.createElement("span");
+        gitLink.setAttribute("class", "action");
+        gitLink.innerHTML = `<a href='${gitUrl}' target='_blank'>View Source Code</a>`;
+        header.insertBefore(gitLink, button);
+    }
+}
+
+function deIndent(code) {
+    let lines = code.split(/\r?\n/);
+    let count = Number.MAX_SAFE_INTEGER;
+    for(var i = 1; i<lines.length; i++){
+        count = Math.min(count, lines[i].search(/[^ ]/));
+    }
+
+    if(count == Number.MAX_SAFE_INTEGER){
+        return code;
+    }
+    else{
+        return lines.map( (line,i) => i==0? line : line.slice(count)).join("\r\n");
     }
 }
